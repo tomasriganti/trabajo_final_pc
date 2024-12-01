9980c09e1ea33e996cba6145c1aec352b6188100
JoaquinPanichelli <jpanichelli@udesa.edu.ar>
2024-12-01 11:19:18 -0300

    auto_car.py

--- car/auto_car.py
+++ car/auto_car.py
@@ -1,10 +1,11 @@
 from car.car import Car
+import math
 
 class AutoCar(Car):
     def __init__(self, driver_name: str, car_number: int):
         super().init(driver_name, car_number)
 
-    def get_command(self, pygame_keys: dict, is_inside_track: bool) -> list[float]:
+    def get_command(self, distances: list, is_inside_track: bool) -> list[float]:
         """
         Returns the command for the car
 
@@ -15,5 +16,36 @@ class AutoCar(Car):
         Returns:
             list[float]: The command [acceleration, steering]
         """
-        self.pygame_keys = pygame_keys
-        self.is_inside_track = is_inside_track
+        self.speed += self.acceleration
+        self.speed = min(self.speed, self.max_speed)
+
+        if distances[0] is None:
+            self.direction -= math.radians(self.steer)
+
+        elif distances[1] is None:
+            self.direction += math.radians(self.steer)
+
+        if distances[0] < 35:
+            self.speed -= self.acceleration
+            self.direction -= math.radians(self.steer)
+
+        elif distances[1] < 35:
+            self.speed -= self.acceleration
+            self.direction += math.radians(self.steer)
+
+        elif distances[0] < 15:
+            self.speed = 0
+            self.direction -= math.radians(self.steer)
+
+        elif distances[1] < 15:
+            self.speed = 0
+            self.direction += math.radians(self.steer)
+
+        else:
+            if distances[0] < distances[1]:  # Girar izquierda
+                self.direction -= math.radians(self.steer)
+
+            if distances[0] > distances[1]:  # Girar derecha
+                self.direction += math.radians(self.steer)
+
+        return [self.acceleration, self.steer]
